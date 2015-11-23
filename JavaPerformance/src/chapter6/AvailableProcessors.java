e21f20ae3e54bc5e745dde8e9e54639b09d27d5b
YouAreGonnaBeFine <mywarcraft@vip.qq.com>
2015-11-23 15:02:30 +0800

    添加注释

--- JavaPerformance/src/chapter6/AvailableProcessors.java
+++ JavaPerformance/src/chapter6/AvailableProcessors.java
@@ -4,6 +4,7 @@ public class AvailableProcessors {
 
 	public static void main(String[] args) {
 		
+		//获得可用处理器数目
 		int availableProcessors = Runtime.getRuntime().availableProcessors();
 		
 		System.out.println(availableProcessors);
