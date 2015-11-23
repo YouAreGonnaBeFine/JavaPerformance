749ed11f93357252c2e5a8d11759f53a363c32fd
YouAreGonnaBeFine <mywarcraft@vip.qq.com>
2015-11-23 15:50:34 +0800

    添加注释

--- JavaPerformance/src/chapter6/AvailableProcessors.java
+++ JavaPerformance/src/chapter6/AvailableProcessors.java
@@ -4,7 +4,7 @@ public class AvailableProcessors {
 
 	public static void main(String[] args) {
 		
-		//获得可用处理器数目
+		//获得可用处理器数目，可据此确定同时并发执行的Java线程数。
 		int availableProcessors = Runtime.getRuntime().availableProcessors();
 		
 		System.out.println(availableProcessors);
