c8da3370a5069488a9a9b3f9fa89a3f72d356f34
YouAreGonnaBeFine <mywarcraft@vip.qq.com>
2015-11-24 13:09:14 +0800

    profiler

---
+++ JavaPerformance/src/chapter6/JprofileTest.java
@@ -0,0 +1,22 @@
+package chapter6;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class JprofileTest {
+	
+	public static void main(String[] args) throws Exception {
+
+		List lis = new LinkedList();
+		while(true){
+            int[] arr = new int[1024 * 8];
+            lis.add(arr);
+			Thread.sleep(10);
+			
+		}
+
+
+
+	}
+
+}
